296741bb9289217523c4ffabdbf08cca5a12f5eb
Jacob-DN-Schmidt <schjacob04@gmail.com>
2026-05-10 23:33:14 -0500

    Revert "Added a working dir check and dir change to standardized output dir location"
    This reverts commit c614c500d438885a97a478107c6306e95102eccd.

--- run.py
+++ run.py
@@ -1,12 +1,4 @@
-import os
 from src import interface_core
 from src import interface_gui
 
-dir = os.path.dirname(__file__)
-cwd = os.getcwd()
-
-if dir != cwd:
-    os.chdir(dir)
-
 interface_gui.begin_interface()
-exit(0)
